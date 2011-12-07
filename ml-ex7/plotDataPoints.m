9614e1987ff8501eed1e8749f42a5f983dc11aae
Amir Chaudhry <amirmc@gmail.com>
2011-12-07 11:41:59 +0000

    updating plotDataPoints (again)

--- ml-ex7/plotDataPoints.m
+++ ml-ex7/plotDataPoints.m
@@ -9,6 +9,8 @@ palette = hsv(K + 1);
 colors = palette(idx, :);
 
 % Plot the data
-scatter(X(:,1), X(:,2), 15, colors);
+% scatter(X(:,1), X(:,2), 15, colors);  % This line hits a bug in the MacOSX version of Octave
+% the following line is a workaround for the bug described above
+colormap(jet); scatter(X(:,1), X(:,2), 15, idx);
 
 end
