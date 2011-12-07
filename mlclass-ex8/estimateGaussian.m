dc42568a0bb5dd31022900d6f3195a5a890d4390
Amir Chaudhry <amirmc@gmail.com>
2011-12-07 14:33:20 +0000

    first attempt at ex1

--- mlclass-ex8/estimateGaussian.m
+++ mlclass-ex8/estimateGaussian.m
@@ -21,16 +21,9 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
-
-
-
-
-
-
-
+mu = mean(X);
+sigma2 = var(X);
 
 % =============================================================
-
 
 end
